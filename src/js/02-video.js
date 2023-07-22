833f9f06fabe16d21e574c06d165568bd9bca81b
YuriiBonislavskiy <130743571+YuriiBonislavskiy@users.noreply.github.com>
2023-07-22 21:51:34 +0300

    ДЗ 8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,38 @@
+const iframe = document.querySelector('iframe');
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const player = new Player(iframe);
+
+const throttled = throttle(() => {
+  getCurrentTime();
+}, 1000);
+
+player.on('play', function () {
+  setCurrentTime();
+});
+
+player.on('timeupdate', function () {
+  throttled();
+});
+
+function getCurrentTime() {
+  player.getCurrentTime().then(function (seconds) {
+    localStorage.setItem('videoplayer-current-time', String(seconds));
+    // console.log(localStorage.getItem('videoplayer-current-time'));
+  });
+}
+
+function setCurrentTime() {
+  const currentTime = localStorage.getItem('videoplayer-current-time');
+  if (currentTime) {
+    player.setCurrentTime(currentTime).then(function (seconds) {
+      // console.log(localStorage.getItem('videoplayer-current-time'));
+    });
+  }
+}
+
+player.unload().then(function () {
+  setCurrentTime();
+});
